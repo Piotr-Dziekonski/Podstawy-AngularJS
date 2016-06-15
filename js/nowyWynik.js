13dc1586c7b51fe86bb240a9f4b423f1262e0059
Piotr-Dziekonski <piotr.dziekon@gmail.com>
2016-06-15 14:52:29 +0200

    uu

--- js/nowyWynik.js
+++
@@ -1,8 +0,0 @@
-function nowyWynik()
-{
-	var przedmiot = document.createElement("div");
-	przedmiot.id = "row";
-	var content = "";
-	przedmiot.appendChild(content);
-	document.getElementById("glownaTresc").appendChild(przedmiot);
-}
